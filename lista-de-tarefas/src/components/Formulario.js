3e7de5295be71b98e1f5e897fc893a0591f874c0
T-pl <tiagofqu@gmail.com>
2022-03-05 18:21:25 -0300

    ECOSITE-003: Adicionei novas propriedades nos componentes

--- lista-de-tarefas/src/components/Formulario.js
+++ lista-de-tarefas/src/components/Formulario.js
@@ -1,22 +1,28 @@
 function Formulario() {
+  function enviarForm(e) {
+    e.preventDefault();
+    alert('Hello, world!');
+  }
   return (
-    <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            O que preciso fazer?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Adicionar
-        </button>
-      </form>
+    <form onSubmit={enviarForm}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          O que preciso fazer?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+      />
+      <button
+        type="submit"
+        className="btn btn__primary btn__lg">
+        Adicionar
+      </button>
+    </form>
   );
 }
 
